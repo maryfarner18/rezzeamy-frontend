e94fd187167d6e1a4da531240d7e0cd1f1654536
maryfarner18 <maryfarner@gmail.com>
2019-12-02 09:59:38 -0500

    setup to split work:

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,14 +1,28 @@
 import React, {Component } from 'react'
+import ProfileContainer from './ProfileContainer'
+import FormContainer from './FormContainer'
 
 class Main extends Component {
     state = [
 
     ]
 
+    renderForm = () =>{
+        return <FormContainer/>
+    }
+
+    renderProfile= () =>{
+        return <ProfileContainer {...routerProps}/>
+    }
+
     render() {
         return (
-            <div>MAIN BODY</div>
-        )
+            <div>
+                <Route path="/:username-slug" render={this.renderProfile}/>
+                <Route path="/setup" render={this.renderForm}/>
+                <Route path="/" render={()=> <div>MAIN BODY</div> }/>
+            </div>
+            )
     }
 }
 
