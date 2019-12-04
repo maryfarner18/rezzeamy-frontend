5a2b13406b1bcd47fe3e5a890623105e4073ea9c
maryfarner18 <maryfarner@gmail.com>
2019-12-04 15:25:59 -0500

    what didnt we do;

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,14 +1,14 @@
 import React, { Component, Fragment } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Icon } from 'semantic-ui-react'
 import { NavLink, Link, Redirect } from 'react-router-dom'
 
 class Nav extends Component {
 
     //state = { activeItem: 'home' } //instead use props of app state
 
-    handleItemClick = (e, { name}) => {
-        this.props.setActive(name)
-    }
+    // handleItemClick = (e, { name}) => {
+    //     this.props.setActive(name)
+    // }
     
     render() {
 
@@ -22,46 +22,33 @@ class Nav extends Component {
                 <Menu.Item
                     as={Link} to='/'
                     name='home'
-                    active={activeItem === 'home'}
-                    onClick={this.handleItemClick}
                 >
-                    Home
+                    <Icon name= "id card" size="big" />
                 </Menu.Item>
+                    <Fragment>
                 {
                 currentUser.user ? 
-                    <Fragment>
+                    <Menu.Menu position='right'>
                         <Menu.Item
-                            as={Link} to={`/:${currentUser.userslug}`}
-                            name='profile'
-                            active={activeItem === 'profile'}
-                            onClick={this.handleItemClick}
-                        >
-                            Profile
-                        </Menu.Item>
-                        <Menu.Menu position='right'>
-                            <Menu.Item
-                            as={Link} to='/'
-                            name='logout'
-                            active={activeItem === 'logout'}
-                            onClick={() => setUser({})}
-                            />
-                        </Menu.Menu>
-                    </Fragment>
+                        as={Link} to='/'
+                        name='logout'
+                        onClick={() => setUser({})}
+                        />
+                    </Menu.Menu>
                 :
-                    <Fragment>
-                        <Menu.Menu position='right'>
-                            <Menu.Item
-                                as={Link} to='/setup'
-                                name='create'
-                                active={activeItem === 'create'}
-                                onClick={this.handleItemClick}
-                            >
-                                Create A Profile!
-                            </Menu.Item>
-                        </Menu.Menu>
-                    </Fragment>
+    
+                    <Menu.Menu position='right'>
+                        <Menu.Item
+                            as={Link} to='/setup'
+                            name='create'
+                        >
+                            Create A Profile!
+                        </Menu.Item>
+                    </Menu.Menu>
 
                 }
+                </Fragment>
+                
             </Menu>
             </div>
         )
